3bc6464fe3c1f49cef676082348b211103799466
kaqu <kaqukal@gmail.com>
2018-04-06 15:59:18 +0200

    linux fix

--- Sources/FuturaAsync/Synchronization/Mutex.swift
+++ Sources/FuturaAsync/Synchronization/Mutex.swift
@@ -16,8 +16,14 @@ public protocol LockProtocol {
     func synchronized<T>(_ closure: () throws -> T) rethrows -> T
 }
 
+#if os(Linux)
+fileprivate let nSecMsec: __time_t = 1_000_000
+fileprivate let mSecSec: __time_t = 1_000 * nSecMsec
+#else
 fileprivate let nSecMsec: __darwin_time_t = 1_000_000
 fileprivate let mSecSec: __darwin_time_t = 1_000 * nSecMsec
+#endif
+
 
 public final class Lock : LockProtocol {
     public let mtx = UnsafeMutablePointer<pthread_mutex_t>.allocate(capacity: 1)
@@ -25,8 +31,8 @@ public final class Lock : LockProtocol {
     public init() {
         let attr = UnsafeMutablePointer<pthread_mutexattr_t>.allocate(capacity: 1)
         guard pthread_mutexattr_init(attr) == 0 else { preconditionFailure() }
-        pthread_mutexattr_settype(attr, PTHREAD_MUTEX_NORMAL)
-        pthread_mutexattr_setpshared(attr, PTHREAD_PROCESS_PRIVATE)
+        pthread_mutexattr_settype(attr, Int32(PTHREAD_MUTEX_NORMAL))
+        pthread_mutexattr_setpshared(attr, Int32(PTHREAD_PROCESS_PRIVATE))
         guard pthread_mutex_init(mtx, attr) == 0 else { preconditionFailure() }
         pthread_mutexattr_destroy(attr)
         attr.deinitialize(count: 1)
@@ -45,7 +51,12 @@ public final class Lock : LockProtocol {
     }
     
     public func lock(timeout: UInt8) throws -> Void {
+        #if os(Linux)
+        var timeout = __time_t(timeout) * mSecSec
+        #else
         var timeout = __darwin_time_t(timeout) * mSecSec
+        #endif
+        
         var rem = timespec(tv_sec: 0, tv_nsec: 0)
         var req = timespec(tv_sec: 0, tv_nsec: 0)
         while pthread_mutex_trylock(mtx) != 0 {
